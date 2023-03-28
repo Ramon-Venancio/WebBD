473cb5761743ee7522c67b22c522edb51d59454b
Ramon-Venancio <ramonvenanciosousa@gmail.com>
2023-03-28 16:48:50 -0300

    criação dos passageiros

--- arline/flights/views.py
+++ arline/flights/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
-from .models import Flight, Airport
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from .models import Flight, Airport, Passenger
 
 
 # Create your views here.
 
 def index(request):
     return render(request, 'flights/index.html', {"flights": Flight.objects.all()})
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
+    return render(request, 'flights/flight.html', {"flight": flight, "passengers": passengers, "non_passengers":non_passengers})
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger_id = int(request.POST.get["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+
+        passenger.flights.add(flight)
+
+        return HttpResponseRedirect(reverse("flight", args=(flight_id)))
